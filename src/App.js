7116461a149bf68f18474137b843cadf1d366332
Bora Manga <bora.mng525@gmail.com>
2020-02-12 11:41:31 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Signup from "./components/Auth/Signup";
+import Signin from "./components/Auth/Signin";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+        <Route path="/signin">
+          <Signin />
+        </Route>
+      </Switch>
     </div>
   );
 }
